33861eb6dc701ede40ab1534ff5aa1d54177a4c4
xiaodi.gou <gouxiaodi@kernelsoft.com>
2022-11-04 18:26:17 +0800

    本地文件

---
+++ MyBatisPlusGenerator/src/test/java/com/xiaodi/qa/mybatisplusgenerator/MyBatisPlusGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaodi.qa.mybatisplusgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisPlusGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
